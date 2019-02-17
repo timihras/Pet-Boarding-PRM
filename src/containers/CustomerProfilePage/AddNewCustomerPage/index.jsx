ebb4eeb5cf060eff9be1c51a3e74e00fb738d062
timihras <timi.hras@gmail.com>
2019-02-17 09:59:12 +0100

    BAsic functionality to add customers

--- src/containers/CustomerProfilePage/AddNewCustomerPage/index.jsx
+++ src/containers/CustomerProfilePage/AddNewCustomerPage/index.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { withFirebase } from '../../../utils/Firebase';
 
-const AddNewCustomerPage = () => {
+import { AuthUserContext } from '../../../utils/Session';
+
+const AddNewCustomerPage = props => {
+  const initialFormValues = {
+    firstName: '',
+    lastName: '',
+    email: ''
+  };
+
+  const [values, setValues] = useState(initialFormValues);
+  const authUser = useContext(AuthUserContext);
+  const { firebase } = props;
+
+  const onChange = e => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    firebase.customers().add({
+      ...values,
+      createdBy: authUser.uid,
+      createdOn: new Date()
+    });
+
+    // alert(JSON.stringify(values));
+
+    setValues(initialFormValues);
+  };
+
   return (
     <div>
       <h1>Add New Customer</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          name="firstName"
+          type="text"
+          placeholder="First name"
+          value={values.firstName}
+          onChange={onChange}
+        />
+        <input
+          name="lastName"
+          type="text"
+          placeholder="Last name"
+          value={values.lastName}
+          onChange={onChange}
+        />
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          value={values.email}
+          onChange={onChange}
+        />
+        <p>
+          <button type="submit">Submit</button>
+        </p>
+      </form>
     </div>
   );
 };
 
-export default AddNewCustomerPage;
+export default withFirebase(AddNewCustomerPage);
--- src/containers/ListsPage/index.jsx
+++ src/containers/ListsPage/index.jsx
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
 
 import { withAuthorization } from '../../utils/Session';
 import { withFirebase } from '../../utils/Firebase';
 
 const ListsPage = () => (
   <div>
-    <h1>Customer & Pet List</h1>
-    <p>In construction...</p>
-
+    <h1>Customer List</h1>
     <CustomerList />
+    <p>
+      <Link to={ROUTES.ADD_CUSTOMER}>Add customer &rarr;</Link>
+    </p>
+    <h1>Pet List</h1>
+    <p>In construction...</p>
   </div>
 );
 
-const CustomerListBase = () => {
-  return <div />;
+const CustomerListBase = props => {
+  const { firebase } = props;
+  const [state, setState] = useState({ loading: true, customers: [] });
+
+  useEffect(() => {
+    firebase.customers().onSnapshot(snapshot => {
+      const customers = [];
+      snapshot.forEach(doc => customers.push({ ...doc.data(), uid: doc.id }));
+      setState({ loading: false, customers });
+    });
+  }, []);
+
+  const { loading, customers } = state;
+
+  return (
+    <div>
+      {loading && <div>Loading...</div>}
+      {customers &&
+        customers.map(customer => (
+          <div key={customer.uid}>
+            {customer.firstName} {customer.lastName}
+          </div>
+        ))}
+    </div>
+  );
 };
 
 const CustomerList = withFirebase(CustomerListBase);
